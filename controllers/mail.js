0d0b5e912fcab5c26599b4a16b2b81a85c1bc907
Pallop <jibbiw123456@gmail.com>
2020-08-29 17:14:33 +0700

    mail feature

---
+++ controllers/mail.js
@@ -0,0 +1,30 @@
+var nodemailer = require('nodemailer')
+
+var botEmail = 'bunnag.bot@gmail.com';
+var botPassword = 'Bottob123!';
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: botEmail,
+      pass: botPassword
+    }
+});
+exports.sendEmail = function(botname, adminEmail, sender, text) {
+    console.log('Start sending mail...');
+    var mailOptions = {
+        from: botEmail,
+        to: adminEmail,
+        subject: 'ข้อความที่บอท ' + botname + ' ไม่สามารถตอบได้',
+        text: 'UserId ' + sender + ': ' + text + '\n\n\n\n\n\n' + 'กรุณาตอบกลับโดยด่วน!!!!!!!!' 
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+    });
+    console.log('End sending mail...');
+}
--- server.js
+++ server.js
@@ -9,69 +9,79 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+
+var mail = require('./controllers/mail')
 // var cors = require('cors')
 
-const adminUserIds = ['U0f5c4c43a6894b7cdb496ba7e200788e'];
+const adminUserIds = [
+  { 
+    userId: 'U0f5c4c43a6894b7cdb496ba7e200788e', 
+    email: 'jibbiw123456@hotmail.com' 
+  }
+];
 
 // app.use(cors())
 app.use(bodyParser.json())
 
 app.set('port', (process.env.PORT || 4000))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 app.get('/', function (req, res) {
-	res.send('Hello')
+  res.send('Hello')
 })
 
 app.post('/webhook', (req, res) => {
-    var text = req.body.events[0].message.text
-    var sender = req.body.events[0].source.userId
-    var replyToken = req.body.events[0].replyToken
-    console.log(text, sender, replyToken)
-    console.log(typeof sender, typeof text)
-    // console.log(req.body.events[0])
-    if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-        sendText(sender, 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞')
-    } else {
-      // TODO: Notification to Admin
-      // jibbiw123456@hotmail.com Pikkoro U0f5c4c43a6894b7cdb496ba7e200788e
-      var userId;
-      for(userId of adminUserIds) {
-        console.log('userId Admin => ' + userId);
-        sendText(userId, 'มีข้อความที่ไม่สามารถตอบได้\n\"' + text + "\"");
-      }
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞')
+  } else {
+    // TODO: Notification to Admin
+    // jibbiw123456@hotmail.com Pikkoro U0f5c4c43a6894b7cdb496ba7e200788e
+    var userId;
+    for (userId of adminUserIds) {
+      console.log('userId Admin => ' + userId['userId']);
+      sendText(userId['userId'], 'มีข้อความที่ไม่สามารถตอบได้\n\"UserID ' + sender + " => " + text + "\"");
+
+      // sendEmail(userId['email'], sender, text)
+      mail.sendEmail('pikkoro', userId['email'], sender, text)
     }
-    res.sendStatus(200)
+  }
+  res.sendStatus(200)
 })
 
-function sendText (sender, text) {
-    let data = {
-      to: sender,
-      messages: [
-        {
-          type: 'text',
-          text: text
-        }
-      ]
-    };
-    console.log('requesting.......................');
-    request({
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {hvvNLdeebyEq4CQDtuKScLo8FxKOnMfJyE+91x2ROY0hT/mZBnVza+vnya1tOSmHrHhB1TOk2MJ14AyOlYpykcGozEOmubDFYj4/F83hOPVyASXwtlTXZHswYVSr7R9PiAT78o+NqBJzutynZLpL7gdB04t89/1O/w1cDnyilFU=}'
-      },
-      url: 'https://api.line.me/v2/bot/message/push',
-      method: 'POST',
-      body: data,
-      json: true
-    }, function (err, res, body) {
-      if (err) console.log('error')
-      if (res) console.log('success')
-      if (body) console.log(body)
-    });
-    console.log('done............................');
+function sendText(sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: text
+      }
+    ]
+  };
+  console.log('requesting.......................');
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {hvvNLdeebyEq4CQDtuKScLo8FxKOnMfJyE+91x2ROY0hT/mZBnVza+vnya1tOSmHrHhB1TOk2MJ14AyOlYpykcGozEOmubDFYj4/F83hOPVyASXwtlTXZHswYVSr7R9PiAT78o+NqBJzutynZLpL7gdB04t89/1O/w1cDnyilFU=}'
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  });
+  console.log('done............................');
 }
 
 app.listen(app.get('port'), function () {
