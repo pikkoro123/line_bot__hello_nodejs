478715685d535cd39000bb9db9a08db1dcf3af3e
Pallop <jibbiw123456@gmail.com>
2020-08-31 21:38:56 +0700

    cors

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ const adminUserIds = [
 // app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+  );
+  next();
+});
+
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
